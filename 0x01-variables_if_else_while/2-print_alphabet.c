f0c8c515d16ebcd0f437973f948249180bfa7226
ohiarre <malikahtaan@gmail.com>
2022-03-11 14:34:16 -0800

    printalphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints combination of numbers
+ * main - Prints the alphabetic
  *
  * Return: Always (Success)
  **/
 int main(void)
 {
-	int c, i;
+		char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		for (i = '0'; i <= '9'; i++)
-		{
-			if (c < i)
-			{
-				putchar(c);
-				putchar(i);
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
 
-				if (c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+		putchar('\n');
+		return (0);
 }
